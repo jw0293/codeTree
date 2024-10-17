804b42c9e5d6a58a6e07b24e65affbacff75791a
jw0293 <80098469+jw0293@users.noreply.github.com>
2024-10-17 23:00:42 +0900

    Title: 개발자의 컴퓨터, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241017/개발자의 컴퓨터/developers-computers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_TIME 100
+
+using namespace std;
+
+int N;
+int computer[100 + 1];
+
+void input(){
+    cin >> N;
+    for(int i=0;i<N;i++){
+        int start, end, cnt;
+        cin >> start >> end >> cnt;
+        for(int j=start;j<=end;j++){
+            computer[j] += cnt;
+        }
+    }
+}
+
+int solution(){
+    int ans = 0;
+    for(int i=1;i<=MAX_TIME;i++){
+        ans = max(ans, computer[i]);
+    }
+    return ans;
+}
+
+int main() {
+    input();
+    cout << solution() << '\n';
+
+    return 0;
+}
