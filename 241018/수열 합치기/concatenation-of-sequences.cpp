36c09f82595c70bde190b3090d54e1229623df04
jw0293 <80098469+jw0293@users.noreply.github.com>
2024-10-18 22:47:43 +0900

    Title: 수열 합치기, Time: 128ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 241018/수열 합치기/concatenation-of-sequences.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> v1, v2;
+
+void input(){
+    cin >> N >> M;
+    v1 = vector<int>(N, 0);
+    v2 = vector<int>(M, 0);
+    for(int i=0;i<N;i++) {
+        cin >> v1[i];
+    }
+    for(int i=0;i<M;i++){
+        cin >> v2[i];
+    }
+}
+
+vector<int> getAllVector(){
+    vector<int> ret;
+    for(auto &cur : v1) ret.push_back(cur);
+    for(auto &cur : v2) ret.push_back(cur);
+    return ret;
+}
+
+void solution(){
+    vector<int> ret = getAllVector();
+    sort(ret.begin(), ret.end());
+    for(auto &p : ret) {
+        cout << p << " ";
+    }
+    cout << '\n';
+}
+
+int main() {
+    input();
+    solution();
+
+    return 0;
+}
